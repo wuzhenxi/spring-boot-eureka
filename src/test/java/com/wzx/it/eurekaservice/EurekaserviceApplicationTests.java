d01290e483ce26f98f86aa06c9b70f6ec1093b55
wuzhenxi <wuzx@hustcad.com>
2019-11-20 11:24:57 +0800

    【type】:新增 【author】:wzx 【description】:增加spring-security

--- src/test/java/com/wzx/it/eurekaservice/EurekaserviceApplicationTests.java
+++ src/test/java/com/wzx/it/eurekaservice/EurekaserviceApplicationTests.java
@@ -1,16 +1,32 @@
 package com.wzx.it.eurekaservice;
 
+import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
+@Slf4j
 public class EurekaserviceApplicationTests {
 
+    @Autowired
+    StringEncryptor stringEncryptor;
+
     @Test
-    public void contextLoads() {
+    public void decrypter() {
+        log.info("[admin]加密后={}", stringEncryptor.encrypt("admin"));
+        log.info("[aSM5t2r6wtNrO0kdwk9Ghw==]解密后={}", stringEncryptor.decrypt("aSM5t2r6wtNrO0kdwk9Ghw=="));
+    }
+
+    @Test
+    public void testGenUUID(){
+        log.info("生成uuid：{}",UUID.randomUUID().toString());
     }
 
 }
