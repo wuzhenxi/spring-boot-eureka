01c1da8049750f5ccfdba89c37fc6e0dcc765ef6
wuzhenxi <wuzx@hustcad.com>
2019-11-27 17:43:23 +0800

    【type】:单元测试test 【author】:wzx 【description】:修改第三次

--- src/main/java/com/wzx/it/eurekaservice/EurekaserviceApplication.java
+++ src/main/java/com/wzx/it/eurekaservice/EurekaserviceApplication.java
@@ -11,6 +11,6 @@ public class EurekaserviceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaserviceApplication.class, args);
     }
-    // 1122第四次修改
+    // 修改第三次
 
 }
